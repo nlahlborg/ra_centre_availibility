4073c304797ac2b164ab629ef9c54af4012e799f
nlahlborg <69569569+nlahlborg@users.noreply.github.com>
2025-02-16 13:43:05 -0800

    close mysql connection on end

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -1,4 +1,3 @@
-import os
 from datetime import datetime, UTC
 from pathlib import Path
 
@@ -8,14 +7,13 @@ from web_scraper.src.upload import get_only_new_data, prepare_transaction, save_
 from web_scraper.src.setup import load_env_file, DB_TZ, db_connect
 
 import logging
-from logging.handlers import TimedRotatingFileHandler
+#from logging.handlers import TimedRotatingFileHandler
 
 load_env_file(str(Path(__file__).parent.parent / ".env"))
-CONN = db_connect()
 
 # Create logs directory if it doesn't exist
-if not os.path.exists('logs'):
-    os.makedirs('logs')
+# if not os.path.exists('logs'):
+#     os.makedirs('logs')
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -27,9 +25,9 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
 # Create file handler which logs even debug messages
-file_handler = TimedRotatingFileHandler('logs/app.log', when='midnight', interval=1, backupCount=7)
-file_handler.setLevel(logging.INFO)
-file_handler.suffix = "%Y-%m-%d"
+# file_handler = TimedRotatingFileHandler('logs/app.log', when='midnight', interval=1, backupCount=7)
+# file_handler.setLevel(logging.INFO)
+# file_handler.suffix = "%Y-%m-%d"
 
 # Create a custom formatter
 class PSTFormatter(logging.Formatter):
@@ -40,11 +38,11 @@ class PSTFormatter(logging.Formatter):
 
 formatter = PSTFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 console_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
+# file_handler.setFormatter(formatter)
 
 # Add the handlers to the logger
 logger.addHandler(console_handler)
-logger.addHandler(file_handler)
+# logger.addHandler(file_handler)
 
 def main():
     #scrape data
@@ -52,15 +50,26 @@ def main():
     response = get_availability()
     data = response[0]["result"]["data"]
     dict_list = parse_availability_data(data)
+    logger.info(f"received {len(dict_list)} items from ra centre site")
 
     #save to mysql
-    data_new = get_only_new_data(dict_list, CONN)
-    n_rows = len(data_new)
-    logger.info(f"Number of new rows for mysql: {n_rows}")
+    conn = db_connect()
+    try:
+        data_new = get_only_new_data(dict_list, conn)
+        n_rows = len(data_new)
+        logger.info(f"Number of new rows for mysql: {n_rows}")
 
-    if n_rows > 0:
-        logger.info("saving data to the mysql database")
-        sql = prepare_transaction(data_new)
-        save_data(sql, CONN)
-    else:
-        logger.info("no new data to save for mysql")
+        if n_rows > 0:
+            logger.info("saving data to the mysql database")
+            sql = prepare_transaction(data_new)
+            save_data(sql, conn)
+        else:
+            logger.info("no new data to save for mysql")
+
+        conn.close()
+
+        return n_rows
+    
+    except Exception as e:
+        conn.close()
+        raise(e)
