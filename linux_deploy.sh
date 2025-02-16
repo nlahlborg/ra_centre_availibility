b3a3b18da514d46f1fad69d3b9eadac02134f4a5
nlahlborg <69569569+nlahlborg@users.noreply.github.com>
2025-02-16 14:56:07 -0800

    use env vars

--- linux_deploy.sh
+++ linux_deploy.sh
@@ -1,6 +1,6 @@
-cd ~/WORKSPACE
+cd $LINUX_ENV_PATH
 python -m venv .linux_venv
 source .linux_venv/bin/activate
-cp "/mnt/c/Users/nadia/windows repos/ra_centre_availibility/requirements.txt" ~/WORKSPACE/requirements.txt
+cp "$WINDOWS_ENV_PATH/requirements.txt" "$LINUX_ENV_PATH/requirements.txt"
 pip install -r requirements.txt
-cp -r .linux_venv/. "/mnt/c/Users/nadia/windows repos/ra_centre_availibility/.linux_venv"
+cp -r .linux_venv/. "$WINDOWS_ENV_PATH/.linux_venv"
