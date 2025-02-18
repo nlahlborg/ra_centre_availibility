7b4c9df3ba249e6b2cc87c6ac10a510d6dd63943
nlahlborg <69569569+nlahlborg@users.noreply.github.com>
2025-02-18 12:14:41 -0800

    don't need __init__

--- web_scraper/deploy.bat
+++ web_scraper/deploy.bat
@@ -1,8 +1,8 @@
+ECHO OFF
 DEL deployment.zip
 POWERSHELL "Compress-Archive -Path '..\.linux_venv\lib\python3.13\site-packages\*' -DestinationPath 'deployment.zip'"
 POWERSHELL "Compress-Archive -Update -Path 'lambda_function.py' -DestinationPath 'deployment.zip'"
 POWERSHELL "Compress-Archive -Update -Path '.env' -DestinationPath 'deployment.zip'"
 POWERSHELL "Compress-Archive -Update -Path 'src' -DestinationPath 'deployment.zip'"
-POWERSHELL "Compress-Archive -Update -Path 'tests' -DestinationPath 'deployment.zip'"
 POWERSHELL "Compress-Archive -Update -Path 'main.py' -DestinationPath 'deployment.zip'"
-POWERSHELL "Compress-Archive -Update -Path '__init__.py' -DestinationPath 'deployment.zip'"
+POWERSHELL "Compress-Archive -Update -Path 'tests' -DestinationPath 'deployment.zip'"
