83963008c3c38f0b42f6cff64e9386a4c3a98cc8
nlahlborg <69569569+nlahlborg@users.noreply.github.com>
2025-04-25 12:18:06 -0700

    refactor secrets

--- web_scraper/deploy_local.bat
+++ web_scraper/deploy_local.bat
@@ -1,6 +1,7 @@
 ECHO OFF
 REM https://docs.aws.amazon.com/lambda/latest/dg/python-image.html#python-image-clients
-SET IMAGE_NAME=ra_centre2
+SETLOCAL EnableDelayedExpansion
+SET IMAGE_NAME=web_scraper_local_test
 SET REGION="us-west-1"
 
 REM stop container if it exists
@@ -10,10 +11,18 @@ docker rm %IMAGE_NAME%_local_test
 REM build image
 docker buildx build --platform linux/amd64 --provenance=false -t docker-image:%IMAGE_NAME%_test .
 REM start container
-docker run --platform linux/amd64 -d -v "%USERPROFILE%\.aws-lambda-rie:/aws-lambda" -p 9000:8080 ^
+docker run -d ^
+    -v "%USERPROFILE%\.aws-lambda-rie:/aws-lambda" -p 9000:8080 ^
+    -e JUMP_HOST="%JUMP_HOST%" ^
+    -e JUMP_USER="%JUMP_USER%" ^
+    -e JUMP_HOST_SSH_KEY="!EC2_SSH_KEY!" ^
+    --platform linux/amd64 ^
     --entrypoint /aws-lambda/aws-lambda-rie ^
     --name %IMAGE_NAME%_local_test^
     --stop-timeout 60 ^
     docker-image:"%IMAGE_NAME%_test" ^
     /usr/local/bin/python -m awslambdaric lambda_function.handler
-powershell -command "Invoke-WebRequest -Uri 'http://localhost:9000/2015-03-31/functions/function/invocations' -Method Post -Body '{\"write_to_db\": 0}' -ContentType 'application/json'"
+
+REM test the lambda function
+TIMEOUT 5
+POWERSHELL -command "Invoke-WebRequest -Uri 'http://localhost:9000/2015-03-31/functions/function/invocations' -Method Post -Body '{\"write_to_db\": 0}' -ContentType 'application/json'"
