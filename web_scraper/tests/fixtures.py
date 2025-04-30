6256909ff59a597273d17f7c5079f67650c297a8
nlahlborg <69569569+nlahlborg@users.noreply.github.com>
2025-04-29 21:10:43 -0700

    add unit test

---
+++ web_scraper/tests/fixtures.py
@@ -0,0 +1,19 @@
+HTML_PAYLOAD_SAMPLE = """
+<!DOCTYPE HTML>
+
+<!--[if lt IE 7]>     <html lang="en" ng-app="app" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>        <html lang="en" ng-app="app" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>        <html lang="en" ng-app="app" class="no-js lt-ie9"> <![endif]-->
+
+<html class="no-js" lang="en" ng-app="app">
+
+    <head>
+<script src="/static/111213/js/perf/stub.js" type="text/javascript"></script><script src="/jslibrary/1745829099254/sfdc/VFRemote.js" type="text/javascript"></script><script>(function(UITheme) {
+    UITheme.getUITheme = function() { 
+        return UserContext.uiTheme;
+    };
+}(window.UITheme = window.UITheme || {}));</script><script type="text/javascript">
+Visualforce.remoting.Manager.add(new $VFRM.RemotingProviderImpl({"vf":{"vid":"0664x00000AprDY","xhr":false,"dev":false,"tst":false,"dbg":false,"tm":1745985402664,"ovrprm":false},"actions":{"ts_avo.AvocadoSiteController":{"ms":[{"name":"dispatch","len":2,"ns":"ts_avo","ver":43.0,"csrf":"VmpFPSxNakF5TlMwd05TMHdNMVF3TXpvMU5qbzBNaTQyTmpWYSxGMFJFNXFzY1E2eFpXN1hDMkswVzlZNkJueWozS2dRMkxwMHYxR1ZjM0VvPSxNMkUyTm1FNQ==","authorization":"eyJub25jZSI6InlGTXNqM2R4akltUTA2elpTa1c0MHdGRGtpdG5Uc1F4RVZQQThpUkZjZVFcdTAwM2QiLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IntcInRcIjpcIjAwRDR4MDAwMDA4WFVNRVwiLFwidlwiOlwiMDJHNHgwMDAwMDB0bHAwXCIsXCJhXCI6XCJ2ZnJlbW90aW5nc2lnbmluZ2tleVwiLFwidVwiOlwiMDA1NHgwMDAwMDdNRFNLXCJ9IiwiY3JpdCI6WyJpYXQiXSwiaWF0IjoxNzQ1OTg1NDAyNjY1LCJleHAiOjB9.Q2lWMGMxOWhkbTh1UVhadlkyRmtiMU5wZEdWRGIyNTBjbTlzYkdWeUxtUnBjM0JoZEdObw==.-A06smnCiG9NXmquUQXy72lsJaMI1GPeDYMjtTbDXeo="}],"prm":0}},"service":"apexremote"}));
+</script><meta HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE" />
+<meta HTTP-EQUIV="Expires" content="Mon, 01 Jan 1990 12:00:00 GMT" />
+"""
---
+++ web_scraper/tests/test_web_query.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import sys
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+from src.web_query import get_context_auth
+from tests.fixtures import HTML_PAYLOAD_SAMPLE
+
+def test_get_context_auth():
+    """
+    unit test the regex from the get_context_auth function
+    """
+    html = HTML_PAYLOAD_SAMPLE
+    action = "ts_avo.AvocadoSiteController"
+
+    result = get_context_auth(html)
+
+    assert result["actions"][action]["ms"][0]["name"] == "dispatch"
+    assert result["actions"][action]["ms"][0]["csrf"] == "VmpFPSxNakF5TlMwd05TMHdNMVF3TXpvMU5qbzBNaTQyTmpWYSxGMFJFNXFzY1E2eFpXN1hDMkswVzlZNkJueWozS2dRMkxwMHYxR1ZjM0VvPSxNMkUyTm1FNQ=="
+    assert result["actions"][action]["ms"][0]["ns"] == "ts_avo"
+    assert int(result["actions"][action]["ms"][0]["ver"]) == 43
+    assert result["vf"]["vid"] == "0664x00000AprDY"
+    assert result["service"] == "apexremote"
+
+if __name__ == "__main__":
+    test_get_context_auth()
